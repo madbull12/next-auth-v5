61859e36fb4f30210f518b2915ae6e95924fa5a4
andriankreasitech <huangandrian02@gmail.com>
2024-02-09 22:02:48 +0700

    testtt

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -1,37 +1,31 @@
-"use client"
-import { useRouter } from 'next/navigation';
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 interface LoginButtonProps {
-    children:React.ReactNode;
-    mode?:"modal" | "redirect";
-    asChild?:boolean;
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
 }
 
 const LoginButton = ({
-    children,
-    mode='redirect',
-    asChild
-}:LoginButtonProps) => {
-
-  const router = useRouter()
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const router = useRouter();
   const redirect = () => {
-    router.push("/auth/login")
-  } 
+    router.push("/auth/login");
+  };
 
-  if(mode==="modal") {
-    return (
-      <span>
-          TODO 
-   
-      </span>
-    )
+  if (mode === "modal") {
+    return <span>TODO</span>;
   }
   return (
-    <span className='cursor-pointer' onClick={redirect}>
-        {children}
+    <span className="cursor-pointer" onClick={redirect}>
+      {children}
     </span>
-  )
-}
+  );
+};
 
-export default LoginButton
+export default LoginButton;
